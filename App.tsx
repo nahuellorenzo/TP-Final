73a825ad826f7cfbaf04e180ccf56c0fb65be28c
Nahuel Lorenzo <nahuelorenzo@gmail.com>
2023-06-16 13:37:01 -0300

    Soluciono nombre de la ruta

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import fonts from "./components/config/fonts";
 import { LoginProvider } from "./components/context/LoginContext";
 
-import AppRouter from "./components/navigation/appRouter";
+import AppRouter from "./components/navigation/AppRouter";
 
 export default function App() {
   const [fontsLoaded] = useFonts(fonts);
