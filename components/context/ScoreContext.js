a6dea15d260ab71b9459ac5590ff37be635e6891
Nahuel Lorenzo <nahuelorenzo@gmail.com>
2023-06-29 00:54:57 -0300

    Agrego recuperacion de score desde firebase

--- components/context/ScoreContext.js
+++ components/context/ScoreContext.js
@@ -1,4 +1,8 @@
 import React, { createContext, useEffect, useState } from "react";
+import { db } from "./../firebase/config"
+import { collection, query, where, addDoc, writeBatch, documentId, getDocs } from "firebase/firestore"
+import { LoginContext } from "./LoginContext";
+import { useContext } from "react";
 
 export const ScoreContext = createContext();
 
@@ -8,6 +12,28 @@ export const ScoreProvider = ({ children }) => {
     incorrect: 0,
   });
 
+  const { user }  = useContext(LoginContext)
+
+
+  useEffect(() => {
+    
+    const fetchScores = async () => {
+      if (user.uid) {
+        const scoreRef = collection(db, 'score');
+        const itemsRef = query(scoreRef, where(documentId(), '==', user.uid));
+        const response = await getDocs(itemsRef);
+
+        const data = response.docs[0].data()
+        console.log(data.scorecorrect)
+        setScore({ correct: data.scorecorrect, incorrect: data.scoreincorrect })
+        console.log("Entré al contexto de scores");
+        
+      }
+    };
+  
+    fetchScores();
+  }, [user.uid]);
+  
   return (
     <ScoreContext.Provider
       value={{
