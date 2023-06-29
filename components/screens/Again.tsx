55249b3550d755a2ecb9d9cb89b1edb71edbea28
Agustín Gomez D'Addario <gomezdaddarioagus@gmail.com>
2023-06-28 23:43:54 -0300

    Arreglo estilo de botones

--- components/screens/Again.tsx
+++ components/screens/Again.tsx
@@ -1,17 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import {
-    Dimensions,
-    Image,
-    SafeAreaView,
-    StyleSheet,
-    Text,
-    TouchableOpacity,
-    View,
-    ToastAndroid,
-    ImageBackground,
-    Modal,
-    Pressable,
-    ScrollView
+  Dimensions,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  ToastAndroid,
+  ImageBackground,
 } from "react-native";
 import Spacing from "../constants/Spacing";
 import FontSize from "../constants/FontSize";
@@ -19,222 +16,118 @@ import Colors from "../constants/Color";
 import Fonts from "../constants/Fonts";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../../types";
+import { LoginContext } from "../context/LoginContext";
+import { ScoreContext } from "../context/ScoreContext";
 const { height } = Dimensions.get("window");
 
 const { width } = Dimensions.get("window");
-
 type Props = NativeStackScreenProps<RootStackParamList, "Again">;
-const [modalVisible, setModalVisible] = useState(false);
+
 const Again: React.FC = ({ navigation: { navigate } }: Props) => {
-    
-    return (
-        <SafeAreaView>
-            <View>
-                <View
-                    style={{
-                        paddingHorizontal: Spacing * 4,
-                        paddingTop: Spacing * 4,
-                    }}
-                >
-                    <Text
-                        style={{
-                            marginTop: Spacing * 10,
-                            fontSize: FontSize.xxLarge,
-                            color: Colors.primary,
-                            fontFamily: Fonts["poppins-bold"],
-                            textAlign: "center",
-                        }}
-                    >
-                        Deseas probar de nuevo?
-                    </Text>
-                </View> 
-                <View
-                    style={{
-                        paddingHorizontal: Spacing * 2,
-                        paddingTop: Spacing * 6,
-                        flexDirection: "row",
-                    }}
-                >
-                    <TouchableOpacity
-                        onPress={() => navigate("MemoryGame")}
-                        style={{
-                            backgroundColor: Colors.primary,
-                            paddingVertical: Spacing * 1.5,
-                            paddingHorizontal: Spacing * 2,
-                            width: "48%",
-                            borderRadius: Spacing,
-                            shadowColor: Colors.primary,
-                            shadowOffset: {
-                                width: 0,
-                                height: Spacing,
-                            },
-                            shadowOpacity: 0.3,
-                            shadowRadius: Spacing,
-                        }}
-                    >
-                        <Text
-                            style={{
-                                fontFamily: Fonts["poppins-bold"],
-                                color: Colors.onPrimary,
-                                fontSize: FontSize.large,
-                                textAlign: "center",
-                            }}
-                        >
-                            Si
-                        </Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        onPress={() => setModalVisible(true)}
-                        style={{
-                            paddingVertical: Spacing * 1.5,
-                            paddingHorizontal: Spacing * 2,
-                            width: "48%",
-                            borderRadius: Spacing,
-                            shadowOffset: {
-                                width: 0,
-                                height: Spacing,
-                            },
-                            shadowOpacity: 0.3,
-                            shadowRadius: Spacing,
-                        }}
-                    >
-                        <Text
-                            style={{
-                                fontFamily: Fonts["poppins-bold"],
-                                color: Colors.text,
-                                fontSize: FontSize.large,
-                                textAlign: "center",
-                            }}
-                        >
-                            No
-                        </Text>
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.centeredView}>
-  <Modal
-    animationType="fade"
-    transparent={true}
-    visible={modalVisible}
-    onRequestClose={() => {
-      setModalVisible(!modalVisible);
-    }}
-  >
-    <View style={styles.modalBackdrop}>
-      <View style={styles.modalView}>
-        <View style={styles.modalContent}>
-          <Text style={styles.modalText}>¿Desea cerrar sesión?</Text>
-          <View style={styles.buttonContainer}>
-            <View style={styles.buttonWrapper}>
-              <Pressable
-                style={[styles.button, styles.buttonClose]}
-                onPress={navigate("Main")}
-              >
-                <Text style={styles.textStyle}>Salir</Text>
-              </Pressable>
-            </View>
-            <View style={styles.buttonWrapper}>
-              <Pressable
-                style={[styles.button, styles.buttonOpen]}
-                onPress={() => setModalVisible(!modalVisible)}
-              >
-                <Text style={styles.textStyle}>Cancelar</Text>
-              </Pressable>
-            </View>
-          </View>
+  const { logout, user } = useContext(LoginContext);
+  const { score } = useContext(ScoreContext)
+  return (
+    <SafeAreaView>
+      <View>
+        <View
+          style={{
+            paddingHorizontal: Spacing * 4,
+            paddingTop: Spacing * 4,
+          }}
+        >
+          <Text
+            style={{
+              marginTop: Spacing * 10,
+              fontSize: FontSize.xxLarge,
+              color: Colors.primary,
+              fontFamily: Fonts["poppins-bold"],
+              textAlign: "center",
+            }}
+          >
+            Deseas probar de nuevo?
+          </Text>
         </View>
+        <View
+          style={{
+            paddingHorizontal: Spacing * 2,
+            paddingTop: Spacing * 6,
+            flexDirection: "row",
+            justifyContent: "space-between", // Agrega esta línea
+          }}
+        >
+          {/* Botón "Si" */}
+          <TouchableOpacity
+            onPress={() => navigate("MemoryGame")}
+            style={{
+              backgroundColor: Colors.primary,
+              paddingVertical: Spacing * 1.5,
+              paddingHorizontal: Spacing * 2,
+              width: "48%",
+              borderRadius: Spacing,
+              shadowColor: Colors.primary,
+              shadowOffset: {
+                width: 0,
+                height: Spacing,
+              },
+              shadowOpacity: 0.3,
+              shadowRadius: Spacing,
+            }}
+          >
+            <Text
+              style={{
+                fontFamily: Fonts["poppins-bold"],
+                color: Colors.onPrimary,
+                fontSize: FontSize.large,
+                textAlign: "center",
+              }}
+            >
+              Si
+            </Text>
+          </TouchableOpacity>
+
+          {/* Botón "No" */}
+          <TouchableOpacity
+            onPress={() => navigate("Main")}
+            style={{
+              paddingVertical: Spacing * 1.5,
+              paddingHorizontal: Spacing * 2,
+              width: "48%",
+              borderRadius: Spacing,
+              shadowOffset: {
+                width: 0,
+                height: Spacing,
+              },
+              shadowOpacity: 0.3,
+              shadowRadius: Spacing,
+            }}
+          >
+            <Text
+              style={{
+                fontFamily: Fonts["poppins-bold"],
+                color: Colors.text,
+                fontSize: FontSize.large,
+                textAlign: "center",
+              }}
+            >
+              No
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        <Text
+          style={{
+            fontSize: FontSize.large,
+            color: Colors.primary,
+            fontFamily: Fonts["poppins-bold"],
+            textAlign: "center",
+            paddingTop: Spacing,
+          }}
+        >
+          Puntaje actual: {score.correct}
+        </Text>
       </View>
-    </View>
-  </Modal>
-</View>
-            </View>
-        </SafeAreaView>
-    );
+    </SafeAreaView>
+  );
 };
 
 export default Again;
-
-const styles = StyleSheet.create({
-    modalContent: {
-      backgroundColor: "grey", // Cambia el color de fondo aquí
-      borderRadius: 5,
-      padding: 30,
-      justifyContent: "center",
-      alignItems: "center",
-      shadowColor: "#000",
-      shadowOffset: {
-        width: 0,
-        height: 2,
-      },
-      shadowOpacity: 0.25,
-      shadowRadius: 4,
-      elevation: 5,
-    },
-  
-    modalBackdrop: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      padding: 10,
-      height: "50%"
-    },
-    centeredView: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      marginTop: 50,
-    },
-    modalView: {
-      margin: 20,
-      backgroundColor: "rgba(255, 255, 255, 0.9)",
-      borderRadius: 5,
-      padding: 50,
-      alignItems: "center",
-      shadowColor: "#000",
-      shadowOffset: {
-        width: 0,
-        height: 2,
-      },
-      shadowOpacity: 0.25,
-      shadowRadius: 4,
-      elevation: 5,
-    },
-    buttonContainer: {
-      flexDirection: "row",
-      width: "100%",
-      justifyContent: "space-around",
-      alignItems: "center",
-      marginTop: 20,
-    },
-    buttonWrapper: {
-      flex: 1,
-      marginHorizontal: 10,
-    },
-    button: {
-      borderRadius: 10,
-      padding: 10,
-      elevation: 2,
-      width: 90,
-      alignSelf: "stretch",
-      flexDirection: "row",
-      justifyContent: "center",
-      alignItems: "center"
-    },
-    buttonOpen: {
-      backgroundColor: "#1F41BB",
-    },
-    buttonClose: {
-      backgroundColor: "#FF0000",
-    },
-    textStyle: {
-      color: "white",
-      fontWeight: "bold",
-      textAlign: "center",
-    },
-    modalText: {
-      marginBottom: 15,
-      textAlign: "center",
-      color: "white",
-      fontWeight: "bold"
-    },
-  });
-  
--- components/screens/MemoryGame.tsx
+++ components/screens/MemoryGame.tsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState, useContext } from "react";
 import {
-    Dimensions,
-    Image,
-    SafeAreaView,
-    StyleSheet,
-    Text,
-    TouchableOpacity,
-    View,
-    ToastAndroid,
-    ImageBackground,
+  Dimensions,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  ToastAndroid,
+  ImageBackground,
 } from "react-native";
 import Spacing from "../constants/Spacing";
 import FontSize from "../constants/FontSize";
@@ -16,185 +16,181 @@ import Colors from "../constants/Color";
 import Fonts from "../constants/Fonts";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../../types";
-import { useIsFocused } from "@react-navigation/native"
+import { useIsFocused } from "@react-navigation/native";
 import { ScoreContext } from "../context/ScoreContext";
 import { imagePaths } from "../constants/Images";
-const { height } = Dimensions.get("window");
 
+const { height } = Dimensions.get("window");
 const { width } = Dimensions.get("window");
 
 type Props = NativeStackScreenProps<RootStackParamList, "MemoryGame">;
 
 const MemoryGame: React.FC = ({ navigation: { navigate } }: Props) => {
-    const [currentImage, setCurrentImage] = useState(null);
-    const [previousImage, setPreviousImage] = useState(null);
-    const isFocused = useIsFocused();
-    const { score, setScore } = useContext(ScoreContext);
+  const [currentImage, setCurrentImage] = useState(null);
+  const [previousImage, setPreviousImage] = useState(null);
+  const isFocused = useIsFocused();
+  const { score, setScore } = useContext(ScoreContext);
 
-    useEffect(() => {
-        // Simulación de carga de la imagen por 10 segundos
-        setPreviousImage(null);
-        const randomImageIndex = Math.floor(Math.random() * imagePaths.length);
-        const randomImagePath = imagePaths[randomImageIndex];
-        setCurrentImage(randomImagePath);
-        console.log(randomImagePath);
-        console.log(require("./../../assets/imagesGame/image1.png"))
-        const timer = setTimeout(() => {
-            // Obtener una nueva imagen al azar de la carpeta de imágenes
-            const newrandomImageIndex = Math.floor(Math.random() * imagePaths.length);
-            const newrandomImagePath = imagePaths[newrandomImageIndex];
-            setPreviousImage(randomImagePath);
-            setCurrentImage(newrandomImagePath);
-        
-        }, 2000);
+  useEffect(() => {
+    setPreviousImage(null);
+    const randomImageIndex = Math.floor(Math.random() * imagePaths.length);
+    const randomImagePath = imagePaths[randomImageIndex];
+    setCurrentImage(randomImagePath);
 
-        return () => clearTimeout(timer);
-    }, [isFocused]);
+    const timer = setTimeout(() => {
+      const newrandomImageIndex = Math.floor(Math.random() * imagePaths.length);
+      const newrandomImagePath = imagePaths[newrandomImageIndex];
+      setPreviousImage(randomImagePath);
+      setCurrentImage(newrandomImagePath);
+    }, 2000);
 
-    const handleOptionSelected = (isSameImage: boolean) => {
-        if (isSameImage) {
-            console.log("Bien es lo correcto")
-            setScore({
-                correct: score.correct + 1,
-                incorrect: score.incorrect,
-              });
-        } else {
-            console.log("Te equivocaste, no es lo correta")
-            setScore({
-                correct: score.correct,
-                incorrect: score.incorrect + 1,
-              });
-        }
-        navigate("Again");
-    };
-    
-    return (
-        <SafeAreaView>
-            <View>
-                {
-                    previousImage === null ? <View
-                    style={{
-                        paddingHorizontal: Spacing * 4,
-                        paddingTop: Spacing * 4,
-                    }}
-                >
-                    <Text
-                        style={{
-                            fontSize: FontSize.xxLarge,
-                            color: Colors.primary,
-                            fontFamily: Fonts["poppins-bold"],
-                            textAlign: "center",
-                        }}
-                    >
-                        Recuerda esta imagen!
-                    </Text>
-                </View> : 
-                <View
-                style={{
-                    paddingHorizontal: Spacing * 4,
-                    paddingTop: Spacing * 4,
-                }}
+    return () => clearTimeout(timer);
+  }, [isFocused]);
+
+  const handleOptionSelected = (isSameImage: boolean) => {
+    if (isSameImage) {
+      console.log("Bien es lo correcto");
+      setScore({
+        correct: score.correct + 1,
+        incorrect: score.incorrect,
+      });
+    } else {
+      console.log("Te equivocaste, no es lo correcto");
+      setScore({
+        correct: score.correct,
+        incorrect: score.incorrect + 1,
+      });
+    }
+    navigate("Again");
+  };
+
+  return (
+    <SafeAreaView>
+      <View>
+        {previousImage === null ? (
+          <View
+            style={{
+              paddingHorizontal: Spacing * 4,
+              paddingTop: Spacing * 4,
+            }}
+          >
+            <Text
+              style={{
+                fontSize: FontSize.xxLarge,
+                color: Colors.primary,
+                fontFamily: Fonts["poppins-bold"],
+                textAlign: "center",
+              }}
             >
-                <Text
-                    style={{
-                        fontSize: FontSize.xxLarge,
-                        color: Colors.primary,
-                        fontFamily: Fonts["poppins-bold"],
-                        textAlign: "center",
-                    }}
-                >
-                    Es la misma Imagen?
-                </Text>
-            </View>
-                }
-                <ImageBackground
-                    style={{
-                        height: height / 2.5,
-                        marginTop: Spacing * 4,
-                    }}
-                    resizeMode="contain"
-                    source={currentImage}
-                />
-                <Text
+              Recuerda esta imagen!
+            </Text>
+          </View>
+        ) : (
+          <View
+            style={{
+              paddingHorizontal: Spacing * 4,
+              paddingTop: Spacing * 4,
+            }}
+          >
+            <Text
+              style={{
+                fontSize: FontSize.xxLarge,
+                color: Colors.primary,
+                fontFamily: Fonts["poppins-bold"],
+                textAlign: "center",
+              }}
+            >
+              ¿Es la misma imagen?
+            </Text>
+          </View>
+        )}
+        <ImageBackground
+          style={{
+            height: height / 2.5,
+            marginTop: Spacing * 4,
+          }}
+          resizeMode="contain"
+          source={currentImage}
+        />
+        <Text
+          style={{
+            fontSize: FontSize.large,
+            color: Colors.primary,
+            fontFamily: Fonts["poppins-bold"],
+            textAlign: "center",
+          }}
+        >
+          Puntaje actual: {score.correct}
+        </Text>
+        {previousImage !== null && (
+          <View
+            style={{
+              paddingHorizontal: Spacing * 2,
+              paddingTop: Spacing * 6,
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
+            <TouchableOpacity
+              onPress={() => handleOptionSelected(currentImage === previousImage)}
+              style={{
+                backgroundColor: Colors.primary,
+                paddingVertical: Spacing * 1.5,
+                paddingHorizontal: Spacing * 2,
+                width: "48%",
+                borderRadius: Spacing,
+                shadowColor: Colors.primary,
+                shadowOffset: {
+                  width: 0,
+                  height: Spacing,
+                },
+                shadowOpacity: 0.3,
+                shadowRadius: Spacing,
+              }}
+            >
+              <Text
                 style={{
-                        fontSize: FontSize.large,
-                        color: Colors.primary,
-                        fontFamily: Fonts["poppins-bold"],
-                        textAlign: "center",
-                    }}
-                >
-                    Puntaje actual: {score.correct}
-                </Text>
-                {
-                    previousImage !== null && (
-                        <View
-                    style={{
-                        paddingHorizontal: Spacing * 2,
-                        paddingTop: Spacing * 6,
-                        flexDirection: "row",
-                    }}
-                >
-                    <TouchableOpacity
-                        onPress={() => handleOptionSelected(currentImage === previousImage)}
-                        style={{
-                            backgroundColor: Colors.primary,
-                            paddingVertical: Spacing * 1.5,
-                            paddingHorizontal: Spacing * 2,
-                            width: "48%",
-                            borderRadius: Spacing,
-                            shadowColor: Colors.primary,
-                            shadowOffset: {
-                                width: 0,
-                                height: Spacing,
-                            },
-                            shadowOpacity: 0.3,
-                            shadowRadius: Spacing,
-                        }}
-                    >
-                        <Text
-                            style={{
-                                fontFamily: Fonts["poppins-bold"],
-                                color: Colors.onPrimary,
-                                fontSize: FontSize.large,
-                                textAlign: "center",
-                            }}
-                        >
-                            Es la misma
-                        </Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        style={{
-                            paddingVertical: Spacing * 1.5,
-                            paddingHorizontal: Spacing * 2,
-                            width: "48%",
-                            borderRadius: Spacing,
-                            shadowOffset: {
-                                width: 0,
-                                height: Spacing,
-                            },
-                            shadowOpacity: 0.3,
-                            shadowRadius: Spacing,
-                        }}
-                    >
-                        <Text
-                            onPress={() => handleOptionSelected(currentImage !== previousImage)}
-                            style={{
-                                fontFamily: Fonts["poppins-bold"],
-                                color: Colors.text,
-                                fontSize: FontSize.large,
-                                textAlign: "center",
-                            }}
-                        >
-                            Es diferente
-                        </Text>
-                    </TouchableOpacity>
-                </View>
-                    )
-                }
-            </View>
-        </SafeAreaView>
-    );
+                  fontFamily: Fonts["poppins-bold"],
+                  color: Colors.onPrimary,
+                  fontSize: FontSize.large,
+                  textAlign: "center",
+                }}
+              >
+                Es la misma
+              </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              onPress={() => handleOptionSelected(currentImage !== previousImage)}
+              style={{
+                paddingVertical: Spacing * 1.5,
+                paddingHorizontal: Spacing * 2,
+                width: "48%",
+                borderRadius: Spacing,
+                shadowOffset: {
+                  width: 0,
+                  height: Spacing,
+                },
+                shadowOpacity: 0.3,
+                shadowRadius: Spacing,
+              }}
+            >
+              <Text
+                style={{
+                  fontFamily: Fonts["poppins-bold"],
+                  color: Colors.text,
+                  fontSize: FontSize.large,
+                  textAlign: "center",
+                }}
+              >
+                Es diferente
+              </Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default MemoryGame;
-
