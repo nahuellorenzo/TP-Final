6a89860b72ec27cc9e839bdea3f3936be919eca9
Nahuel Lorenzo <nahuelorenzo@gmail.com>
2023-09-23 17:53:31 -0300

    Agrego dropdown con niveles

---
+++ components/screens/Dropdown.tsx
@@ -0,0 +1,109 @@
+import React, { useState } from 'react';
+  import { StyleSheet, View, Text } from 'react-native';
+  import { Dropdown } from 'react-native-element-dropdown';
+  import AntDesign from '@expo/vector-icons/AntDesign';
+  import Colors from "../constants/Color";
+
+  const data = [
+    { label: 'Facil', value: 'Facil' },
+    { label: 'Intermedio', value: 'Intermedio' },
+    { label: 'Dificil', value: 'Dificil' },
+  ];
+
+  interface DropdownProps {
+    onValueChange: (value: string | null) => void; // Prop que acepta la función de devolución de llamada
+  }
+
+  const DropdownComponent = ({ onValueChange }) => {
+    const [value, setValue] = useState(null);
+
+    const renderItem = item => {
+      return (
+        <View style={styles.item}>
+          <Text style={styles.textItem}>{item.label}</Text>
+          {item.value === value && (
+            <AntDesign
+              style={styles.icon}
+              color="black"
+              name="Safety"
+              size={20}
+            />
+          )}
+        </View>
+      );
+    };
+
+    return (
+      <Dropdown
+        style={styles.dropdown}
+        placeholderStyle={styles.placeholderStyle}
+        selectedTextStyle={styles.selectedTextStyle}
+        iconStyle={styles.iconStyle}
+        data={data}
+
+        maxHeight={300}
+        labelField="label"
+        valueField="value"
+        placeholder="Selecciona..."
+
+        value={value}
+        onChange={item => {
+          setValue(item.value);
+          onValueChange(item.value);
+        }}
+        renderLeftIcon={() => (
+          <AntDesign style={styles.icon} color="black" name="Safety" size={20} />
+        )}
+        renderItem={renderItem}
+      />
+    );
+  };
+
+  export default DropdownComponent;
+
+  const styles = StyleSheet.create({
+    dropdown: {
+      margin: 16,
+      height: 50,
+      backgroundColor: 'white',
+      borderRadius: 12,
+      activeColor: Colors.primary,
+      padding: 12,
+      shadowColor: '#000',
+      shadowOffset: {
+        width: 0,
+        height: 1,
+      },
+      shadowOpacity: 0.2,
+      shadowRadius: 1.41,
+
+      elevation: 2,
+    },
+    icon: {
+      marginRight: 5,
+    },
+    item: {
+      padding: 17,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+    textItem: {
+      flex: 1,
+      fontSize: 16,
+    },
+    placeholderStyle: {
+      fontSize: 16,
+    },
+    selectedTextStyle: {
+      fontSize: 16,
+    },
+    iconStyle: {
+      width: 20,
+      height: 20,
+    },
+    inputSearchStyle: {
+      height: 40,
+      fontSize: 16,
+    },
+  });
