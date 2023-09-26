dfb1485ca6e9b181376e565b1ef4f585dc176455
Agustín Gomez D'Addario <gomezdaddarioagus@gmail.com>
2023-09-26 11:51:53 -0300

    Soluciono error del logout

--- components/screens/UserProfile.tsx
+++ components/screens/UserProfile.tsx
@@ -93,15 +93,39 @@ function UserProfileScreen({ navigation: { navigate } }: Props) {
         </View>
 
         <Modal
-          animationType="fade"
-          transparent={true}
-          visible={modalVisible}
-          onRequestClose={() => {
-            Alert.alert("Modal has been closed.");
-            setModalVisible(!modalVisible);
-          }}
-        >
-          {/* ... Resto del código del modal */}
+              animationType="fade"
+              transparent={true}
+              visible={modalVisible}
+              onRequestClose={() => {
+                Alert.alert("Modal has been closed.");
+                setModalVisible(!modalVisible);
+              }}
+            >
+              <View style={styles.modalBackdrop}>
+                <View style={styles.modalView}>
+                  <View style={styles.modalContent}>
+                    <Text style={styles.modalText}>¿Desea cerrar sesión?</Text>
+                    <View style={styles.buttonContainer}>
+                      <View style={styles.buttonWrapper}>
+                        <Pressable
+                          style={[styles.button, styles.buttonClose]}
+                          onPress={logout}
+                        >
+                          <Text style={styles.textStyle}>Salir</Text>
+                        </Pressable>
+                      </View>
+                      <View style={styles.buttonWrapper}>
+                        <Pressable
+                          style={[styles.button, styles.buttonOpen]}
+                          onPress={() => setModalVisible(!modalVisible)}
+                        >
+                          <Text style={styles.textStyle}>Cancelar</Text>
+                        </Pressable>
+                      </View>
+                    </View>
+                  </View>
+                </View>
+              </View>
         </Modal>
       </View>
     </SafeAreaView>
@@ -109,6 +133,88 @@ function UserProfileScreen({ navigation: { navigate } }: Props) {
 }
 
 const styles = StyleSheet.create({
+  modalContent: {
+    backgroundColor: "grey", // Cambia el color de fondo aquí
+    borderRadius: 5,
+    padding: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+
+  modalBackdrop: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
+    height: "50%"
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 50,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "rgba(255, 255, 255, 0.9)",
+    borderRadius: 5,
+    padding: 50,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-around",
+    alignItems: "center",
+    marginTop: 20,
+  },
+  buttonWrapper: {
+    flex: 1,
+    marginHorizontal: 10,
+  },
+  button: {
+    borderRadius: 10,
+    padding: 10,
+    elevation: 2,
+    width: 90,
+    alignSelf: "stretch",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  buttonOpen: {
+    backgroundColor: "#1F41BB",
+  },
+  buttonClose: {
+    backgroundColor: "#FF0000",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+    color: "white",
+    fontWeight: "bold"
+  },
   container: {
     flex: 1,
   },
@@ -141,12 +247,6 @@ const styles = StyleSheet.create({
     borderRadius: Spacing,
     textAlign: "center",
     padding: Spacing * 2,
-  },
-  buttonContainer: {
-    marginTop: Spacing,
-    flexDirection: "row",
-    justifyContent: "flex-end",
-    alignItems: "flex-end",
   },
   logoutButton: {
     marginVertical: Spacing * 2,
