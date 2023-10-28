fb9a5128aa45b6365f40b35a472c3671318fcace
Agustín Gomez D'Addario <gomezdaddarioagus@gmail.com>
2023-10-28 16:02:14 -0300

    Agrego links

--- components/screens/Information.tsx
+++ components/screens/Information.tsx
@@ -3,21 +3,32 @@ import {
     Text,
     TouchableOpacity,
     View,
-    ScrollView
-} from "react-native";
-import Spacing from "../constants/Spacing";
-import FontSize from "../constants/FontSize";
-import Colors from "../constants/Color";
-import Fonts from "../constants/Fonts";
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { RootStackParamList } from "../../types";
-import { StyleSheet, Button } from 'react-native';
-import { Video, ResizeMode } from 'expo-av';
+    ScrollView,
+    Linking
+} from 'react-native';
+import Spacing from '../constants/Spacing';
+import FontSize from '../constants/FontSize';
+import Colors from '../constants/Color';
+import Fonts from '../constants/Fonts';
+import { FontAwesome } from '@expo/vector-icons'; 
+import { Octicons } from '@expo/vector-icons';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../../types';
 
 type Props = NativeStackScreenProps<RootStackParamList, "Information">;
 const InformationScreen: React.FC<Props> = ({ navigation: { navigate } }: Props) => {
-    const video = React.useRef(null);
-    const [status, setStatus] = React.useState({ isPlaying: false });
+    const openYouTubeVideo = () => {
+        const youtubeVideoURL = 'https://youtu.be/TbeNCyrhYMM';
+
+        Linking.openURL(youtubeVideoURL)
+            .catch((err) => console.error('Error al abrir el enlace: ', err));
+    };
+    const openPaper = () => {
+        const paperURL = 'https://drive.google.com/file/d/1bRSpxDZUDBlZM8W7LrvTtr3RnCXEOtOX/view?usp=sharing';
+
+        Linking.openURL(paperURL)
+            .catch((err) => console.error('Error al abrir el enlace: ', err));
+    };
     return (
         <ScrollView>
             <View
@@ -51,34 +62,41 @@ const InformationScreen: React.FC<Props> = ({ navigation: { navigate } }: Props)
                     >
                         Es la capacidad del cerebro de almacenar y manipular temporalmente información para el desempeño de tareas complejas. Funciona como una almacén temporal que mantiene información para actividades como resolver problemas, tomar decisiones o comprender situaciones complejas.
                     </Text>
-                    <br></br>
-                    <Video
-                        ref={video}
-                        style={styles.video}
-                        source={{
-                            uri: "https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
-                        }}
-                        useNativeControls
-                        resizeMode={ResizeMode.CONTAIN}
-                        isLooping
-                        onPlaybackStatusUpdate={status => {
-                            if ('isLoaded' in status && status.isLoaded) {
-                                setStatus({ isPlaying: status.isPlaying });
-                            } else if ('error' in status) {
-                                console.error(`Error: ${status.error}`);
-                            }
-                        }}
-
-                    />
-                    <View style={styles.buttons}>
-                        <Button
-                            title={status.isPlaying ? 'Pausar' : 'Reproducir'}
-                            color={Colors.primary}
-                            onPress={() =>
-                                status.isPlaying ? video.current?.pauseAsync() : video.current?.playAsync()
-                            }
-                        />
+                    <View style={{ alignContent: 'center', alignSelf: 'center' }}>
                     </View>
+                    <TouchableOpacity
+                        onPress={() => openYouTubeVideo()}
+                        style={{
+                            padding: Spacing * 2,
+                            backgroundColor: Colors.primary,
+                            marginVertical: Spacing * 4,
+                            borderRadius: Spacing,
+                            shadowColor: Colors.gray,
+                            shadowOffset: {
+                                width: 0,
+                                height: Spacing,
+                            },
+                            shadowOpacity: 0.3,
+                            shadowRadius: Spacing,
+                            display: "flex",
+                            flexDirection: "row",
+                            justifyContent: "flex-start",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Octicons name="video" size={Spacing * 4} color={Colors.onPrimary} style={{ textAlign: "justify" }} />
+                        <Text
+                            style={{
+                                fontFamily: Fonts["Roboto-Bold"],
+                                color: Colors.onPrimary,
+                                textAlign: "center",
+                                fontSize: FontSize.large,
+                                paddingLeft: Spacing * 6,
+                            }}
+                        >
+                            Mirar video Informativo
+                        </Text>
+                    </TouchableOpacity>
                     <Text
                         style={{
                             fontSize: FontSize.large,
@@ -97,12 +115,45 @@ const InformationScreen: React.FC<Props> = ({ navigation: { navigate } }: Props)
                             textAlign: "left",
                         }}
                     >
-                        Porque gracias a ella, podemos desarrollar tareas como: <br></br>
-                        - Seguir el hilo de una conversación <br></br>
-                        - Nos permite aprender algo y asociarlo con lo que ya sabemos <br></br>
-                        - Mantener en la mente cierta información necesaria para el desarrollo de una tarea <br></br>
+                        Porque gracias a ella, podemos desarrollar tareas como:
+                        - Seguir el hilo de una conversación
+                        - Nos permite aprender algo y asociarlo con lo que ya sabemos
+                        - Mantener en la mente cierta información necesaria para el desarrollo de una tarea
                         La memoria del trabajo es esencial en tareas como seguir los pasos de una receta o recordar lo que tenemos que comprar en el supermercado
                     </Text>
+                    <TouchableOpacity
+                        onPress={() => openPaper()}
+                        style={{
+                            padding: Spacing * 2,
+                            backgroundColor: Colors.primary,
+                            marginVertical: Spacing * 4,
+                            borderRadius: Spacing,
+                            shadowColor: Colors.gray,
+                            shadowOffset: {
+                                width: 0,
+                                height: Spacing,
+                            },
+                            shadowOpacity: 0.3,
+                            shadowRadius: Spacing,
+                            display: "flex",
+                            flexDirection: "row",
+                            justifyContent: "flex-start",
+                            alignItems: "center",
+                        }}
+                    >
+                        <FontAwesome name="newspaper-o" size={Spacing * 4} color={Colors.onPrimary} style={{ textAlign: "justify" }} />
+                        <Text
+                            style={{
+                                fontFamily: Fonts["Roboto-Bold"],
+                                color: Colors.onPrimary,
+                                textAlign: "center",
+                                fontSize: FontSize.large,
+                                paddingLeft: Spacing * 6,
+                            }}
+                        >
+                            Leer el paper Informativo
+                        </Text>
+                    </TouchableOpacity>
                 </View>
             </View>
         </ScrollView>
@@ -111,34 +162,3 @@ const InformationScreen: React.FC<Props> = ({ navigation: { navigate } }: Props)
 
 
 export default InformationScreen;
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        backgroundColor: '#ecf0f1',
-    },
-    video: {
-        alignSelf: 'center',
-        width: 320,
-        height: 200,
-        marginBottom: Spacing,
-        marginTop: Spacing
-    },
-    buttons: {
-        padding: Spacing * 3,
-        backgroundColor: Colors.primary,
-        marginVertical: Spacing * 4,
-        borderRadius: Spacing,
-        shadowColor: Colors.primary,
-        shadowOffset: {
-            width: 0,
-            height: Spacing,
-        },
-        shadowOpacity: 0.3,
-        shadowRadius: Spacing,
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-    }
-});
