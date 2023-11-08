8ec6b0470fda5347c583e9bfd5b062b32253631f
Agustín Gomez D'Addario <gomezdaddarioagus@gmail.com>
2023-11-08 20:00:05 -0300

    Agrego mejoras visuales

--- components/screens/Tutorial1.tsx
+++ components/screens/Tutorial1.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, ScrollView,Text } from 'react-native';
 import Onboarding from 'react-native-onboarding-swiper';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../types';
@@ -18,29 +18,29 @@ const Tutorial1Screen: React.FC<Props> = ({ navigation: { navigate } }: Props) =
                         backgroundColor: '#fff',
                         image: <Image source={imagePathsTuto[0]}
                         style={{width: 300, height: 350}}/>,
-                        title: "Muestra del patrón de números",
-                        subtitle: 'Primero vas a tener unos segundos para memorizar la secuencia de números',
+                        title: <ScrollView style={{marginTop:-50}}><View style={{height:350}}><Text style={{fontSize: FontSize.xLarge, color: Colors.primary, fontFamily: Fonts["poppins-bold"], textAlign: "center"}}>Primero vas a tener unos segundos para memorizar la secuencia de números</Text></View></ScrollView>,
+                        subtitle: "",
                     },
                     {
                         backgroundColor: '#fff',
                         image: <Image source={imagePathsTuto[1]}
-                        style={{width: 350, height: 400}} />,
-                        title: 'Entre los números',
-                        subtitle: 'Entre número y número vas a tener unos segundos de espera para hacerlo más difícil',
+                        style={{width: 300, height: 350}} />,
+                        title: <ScrollView style={{marginTop:-50}}><View style={{height:350}}><Text style={{fontSize: FontSize.xLarge, color: Colors.primary, fontFamily: Fonts["poppins-bold"], textAlign: "center"}}>Entre número y número vas a tener unos segundos de espera para hacerlo más difícil</Text></View></ScrollView>,
+                        subtitle: "",
                     },
                     {
                         backgroundColor: '#fff',
                         image: <Image source={imagePathsTuto[2]}
-                        style={{width: 350, height: 400}} />,
-                        title: 'Respuesta',
-                        subtitle: "Ahora tendrás que ingresar el número que lograste memorizar, tenés 5 intentos",
+                        style={{width: 300, height: 350}} />,
+                        title: <ScrollView style={{marginTop:-50}}><View style={{height:350}}><Text style={{fontSize: FontSize.xLarge, color: Colors.primary, fontFamily: Fonts["poppins-bold"], textAlign: "center"}}>Ahora tendrás que ingresar el número que lograste memorizar, tenés 5 intentos</Text></View></ScrollView>,
+                        subtitle: "",
                     },
                     {
                         backgroundColor: '#fff',
                         image: <Image source={imagePathsTuto[3]}
-                        style={{width: 270, height: 380}} />,
-                        title: 'Resultados',
-                        subtitle: "Finalmente podrás ver cuál era la respuesta correcta. Si no lograste memorizar, no pasa nada. Intentá las veces que quieras!!",
+                        style={{width: 270, height: 330}} />,
+                        title: <ScrollView style={{marginTop:-50}}><View style={{height:400}}><Text style={{fontSize: FontSize.xLarge, color: Colors.primary, fontFamily: Fonts["poppins-bold"], textAlign: "center"}}>Finalmente podrás ver cuál era la respuesta correcta. Si no lograste memorizar, no pasa nada. Intentá las veces que quieras!!</Text></View></ScrollView>,
+                        subtitle: "",
                     },
                 ]}
                 bottomBarColor={Colors.onPrimary}
@@ -50,10 +50,11 @@ const Tutorial1Screen: React.FC<Props> = ({ navigation: { navigate } }: Props) =
                 nextLabel={'Siguiente'}
                 onDone={() => navigate('InstruccionesJuego2')}
                 flatlistProps 
+                bottomBarHeight={50}
                 transitionAnimationDuration={200}
-                containerStyles={{marginTop: -100}}
-                titleStyles={{marginTop: -50, fontSize: FontSize.xLarge, color: Colors.primary, fontFamily: Fonts["poppins-bold"], textAlign: "center"}}
-                subTitleStyles={{fontSize: FontSize.large, color: Colors.primary, fontFamily: Fonts["poppins-bold"], textAlign: "center"}}
+                containerStyles={{marginTop: 20}}
+                //titleStyles={{marginTop: -50, fontSize: FontSize.xLarge, color: Colors.primary, fontFamily: Fonts["poppins-bold"], textAlign: "center"}}
+                //subTitleStyles={{fontSize: FontSize.large, color: Colors.primary, fontFamily: Fonts["poppins-bold"], textAlign: "center", marginBottom: 30}}
             />
     );
 };
